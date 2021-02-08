8d9f5b362bd9e6b59ae2068740eee0b556667a89
devatearth <64774809+devatearth@users.noreply.github.com>
2021-02-08 13:04:09 +0530

    Added css for Login button on Header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,15 +1,21 @@
-import React,{Component} from 'react';
-import './Header.css';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import "./Header.css";
+import Button from "@material-ui/core/Button";
 
-
-class Header extends Component{
-    render(){
-        return(
-            <div><Button variant="contained" color="inherit">Login </Button>
-            </div>
-        );
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="app-header">
+          <div className="login-button">
+            <Button variant="contained" color="default">
+              Login
+            </Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Header;
